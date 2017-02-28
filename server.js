380c553b10be0abfa17a8ef2bc4a16ecfa572446
Monserrat Genereux <monserratgenereux@gmail.com>
2017-02-27 21:40:49 -0600

    server and routes

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const api = require('./router');
+
+const app = express();
+
+//MiddleWare
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+api.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next();
+});
+
+//REST functions
+app.use('/api', api);
+
+/////////////////////////////////////
+
+//Connection
+mongoose.connect("mongodb://localhost:27017/ProyectoMoviles", function(err,db){
+    if(!err){
+        console.log("we are connected to mongo");
+    }
+})
+
+app.get('/', function(req, res){
+	res.send('Movile API');
+});
+const server = app.listen(port, function(){
+    //Response of try to start server
+    console.log("Listening on port", server.address().port )
+})
