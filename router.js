b55dcee2e0696f71f4028d65ffa0ced97f4a2d33
Monserrat Genereux <monserratgenereux@gmail.com>
2017-02-28 01:04:47 -0600

    changeNames

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 let router = require('express').Router();
-const Items = require('./app/models/product');
+const Product = require('./app/models/product');
 
 //GET ALL
 findAllItems = function(req, res) {
@@ -29,6 +29,25 @@ findByID = function(req, res) {
 addItems = function(req, res) {
   let items = new Items({
     name: req.body.name,
+findAllProduct = function (req, res){
+  Product.find(function(err, product){
+    if(!err) res.send(product);
+	else console.log ('ERROR: ' + err);
+	});
+};
+
+//GET
+findByID = function (req, res){
+  Product.findById(req.params._id, function(err, product){
+    if(!err) res.send(product);
+	else console.log ('ERROR: ' + err);
+	});
+};
+
+//POST
+addProduct = function(req,res){
+  var product = new Product({
+	name: req.body.name,
     cost: req.body.cost,
     store: req.body.store,
     description: req.body.description
@@ -43,6 +62,11 @@ addItems = function(req, res) {
     }
   });
   res.send(items);
+	product.save(function(err){
+	  if(!err)console.log('Product saved');
+	  else console.log('ERROR: ' + err)
+	});
+	res.send(product);
 };
 
 //PUT (update)
@@ -72,14 +96,35 @@ deleteItem = function(req, res) {
       if (!err) console.log('item deleted');
       else console.log('ERROR: ' + err);
     })
+updateProduct = function(req,res){
+  Product.findById(req.body._id, function(err,product){
+    product.name= req.body.name,
+	product.cost= req.body.cost,
+	product.store= req.body.store,
+	product.description= req.body.description
+
+	product.save(function(err){
+	  if(!err) console.log('Product updated');
+	  else console.log('ERROR: ' + err);
+	  });
+	});	
+};
+
+//DELETE
+deleteProduct = function (req,res){
+  Product.findByID(req.body._id, function(err, product){
+    product.remove(function(err){
+	  if(!err) console.log('Product deleted');
+	  else console.log('ERROR: ' + err);
+	})
   });
 };
 
 //API ROUTES
-router.get('/models/product', findAllItems);
+router.get('/models/product', findAllProduct);
 router.get('/models/product/:_id', findByID);
-router.post('/models/product', addItems);
-router.put('/models/product', updateItems);
-router.delete('/models/product', deleteItem);
+router.post('/models/product', addProduct);
+router.put('/models/product', updateProduct);
+router.delete('/models/product', deleteProduct);
 
 module.exports = router;
